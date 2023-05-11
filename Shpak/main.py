5e0e5391dc37005de05b6058f1d0b74178bd1d14
ShpKir <127387808+ShpKir@users.noreply.github.com>
2023-05-11 11:07:21 +0300

    Add files via upload

---
+++ Shpak/main.py
@@ -0,0 +1,22 @@
+from fun_input import fun_input
+#     розмір масиву
+n = int(input())
+
+#     створення масиву
+arr = []
+fun_input(arr, n)
+
+#for i in range(0, n):
+#    arr.append(int(input()))
+
+#     чи є нуль нулів
+flag = True
+for i in range(0, n):
+    if arr[i] == 0:
+        flag = False
+
+#     виведення
+if flag:
+    print("нема")
+else:
+    print("є")
